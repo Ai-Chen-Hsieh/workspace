df93874a6cdfa5d39686163644b081877f4c6a51
Ai-Chen-Hsieh <a0903109727@gmail.com>
2024-10-07 09:35:13 +0800

    feat: login page

--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,30 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MaterialModule } from '../../shared/material/material';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { SnackbarService } from '../../service/snackbar.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [MaterialModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  constructor(private builder: FormBuilder, private route: Router) {
-   
-  }
+
+  private _snackBar = inject(SnackbarService);
+  private _builder = inject(FormBuilder);
+  private _route = inject(Router);
+
+  loginform!: FormGroup;
+  
   ngOnInit(): void {
-   this.loginform = this.builder.group({
-      username: this.builder.control('', Validators.required),
-      password: this.builder.control('', Validators.required)
+   this.loginform = this._builder.group({
+      username: this._builder.control('', Validators.required),
+      password: this._builder.control('', Validators.required)
     })
   }
 
-  loginform: any;
-
-  Proceedlogin(){
-    this.route.navigateByUrl('home')
+  login(){
+    if(this.loginform.invalid){
+      this._snackBar.error('login Error!! please check your username and password')
+      return
+    } else {
+      this._snackBar.success('login Success!!')
+    }
+    
+    this._route.navigateByUrl('home')
   }
-  resetlogin(){}
 }
