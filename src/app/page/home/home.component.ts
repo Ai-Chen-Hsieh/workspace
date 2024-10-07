48277d52190fbaa1f0689ee4a3c0d45dd3718e36
Ai-Chen-Hsieh <a0903109727@gmail.com>
2024-10-07 09:34:59 +0800

    feat: home page

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -10,7 +10,7 @@ import { TodoListComponent } from '../../components/todo-list/todo-list.componen
   standalone: true,
   imports: [MaterialModule, ReactiveFormsModule, RouterModule, TodoListComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
