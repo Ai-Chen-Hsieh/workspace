919d46d95198bb51d2d3d2e4156156d20b0b21b7
Ai-Chen-Hsieh <a0903109727@gmail.com>
2024-10-09 21:34:09 +0800

    feat: 加入 dark mode

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, ViewChild } from "@angular/core";
+import { Component, effect, inject, signal, ViewChild } from "@angular/core";
 import { MaterialModule } from "../shared/material/material";
 import { Router, RouterModule, RouterOutlet } from "@angular/router";
 import { BreakpointObserver } from "@angular/cdk/layout";
@@ -12,17 +12,19 @@ import { MatSidenav } from "@angular/material/sidenav";
     <router-outlet></router-outlet>
   }
   @else {
-
     <mat-toolbar class="toolbar">
       <button mat-icon-button (click)="toggleMenu()">
         <mat-icon>menu</mat-icon>
       </button>
-      <span style="margin-left: 1.5em">My Todo App </span>
-      <div class="container">
-      <h1 class="title">1223</h1>
-    </div>
-      <button mat-button routerLink="/login" class="logout">Logout</button>
-    
+      <span style="margin-left: 1.5em">My Work Space </span>
+      <button mat-icon-button (click)="isDark.set(!isDark())" style="margin: 0 0.5em 0 auto">
+        @if(isDark()){
+          <mat-icon>dark_mode</mat-icon>
+        } @else {
+          <mat-icon>light_mode</mat-icon>
+        }
+      </button>
+      <button mat-icon-button routerLink="/login" class="logout"><mat-icon>logout</mat-icon></button>
     </mat-toolbar>
     <mat-sidenav-container class="example-container">
       <mat-sidenav class="sideNav" [ngClass]="!isCollapsed ? 'expanded' : ''" [mode]="isMobile ? 'over' : 'side'" [opened]="isMobile ? 'false' : 'true'">
@@ -32,7 +34,7 @@ import { MatSidenav } from "@angular/material/sidenav";
                 <a routerLink={{item.menu}} class="list-item" (click)="closeMenu()">
                     <mat-icon>{{item.icon}}</mat-icon>
                     @if (!isCollapsed) {
-                      <span class="item-name">{{item.menu}}</span>
+                      <span>{{item.menu}}</span>
                     }
                 </a>
             </mat-list-item>
@@ -62,11 +64,25 @@ export class PageComponent {
   ];
 
   router = inject(Router);
+
+  // menu toggle
   observer = inject(BreakpointObserver);
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
   isMobile= true;
   isCollapsed = true;
+
+  //dark mode change
+  isDark = signal(false);
+
+  setDarkMode = effect(() => {
+    const htmlEl = document.documentElement;
+    if (this.isDark()) {
+      htmlEl.classList.add('dark');
+    } else {
+      htmlEl.classList.remove('dark');
+    }
+  })
 
   ngOnInit(): void {
     this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
